a7b63a3772dcb864dcc850f385de2a1de3086950
Dejan <balabandejan633@gmail.com>
2022-04-29 10:03:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const user = {
+  username: "new_user",
+  password: "123456789"
+};
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const message = document.getElementById("message");
+const form = document.getElementById("form");
+const checkUsername = document.getElementsByClassName("check-username");
+const failedUsername = document.getElementsByClassName("failed-username");
+const checkPassword = document.getElementsByClassName("check-password");
+const failedPassword = document.getElementsByClassName("failed-password");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let message = [];
+
+  if (username.value === "") {
+    message.push("*please, enter username");
+    checkUsername[0].classList.remove("show-check-username");
+    failedUsername[0].classList.add("show-failed-username");
+    username.classList.remove("success");
+    username.classList.add("error");
+  }
+  if (username.value !== user.username && username.value !== "") {
+    message.push("*please, enter valid username");
+    checkUsername[0].classList.remove("show-check-username");
+    failedUsername[0].classList.add("show-failed-username");
+    username.classList.remove("success");
+    username.classList.add("error");
+  }
+
+  if (password.value === "") {
+    message.push("*please, enter password");
+    checkPassword[0].classList.remove("show-check-password");
+    failedPassword[0].classList.add("show-failed-password");
+    password.classList.remove("success");
+    password.classList.add("error");
+  }
+  if (password.value !== user.password && password.value !== "") {
+    message.push("*please, enter valid password");
+    checkPassword[0].classList.remove("show-check-password");
+    failedPassword[0].classList.add("show-failed-password");
+    password.classList.remove("success");
+    password.classList.add("error");
+  }
+
+  if (username.value === user.username) {
+    failedUsername[0].classList.remove("show-failed-username");
+    checkUsername[0].classList.add("show-check-username");
+    username.classList.remove("error");
+    username.classList.add("success");
+  }
+
+  if (password.value === user.password) {
+    failedPassword[0].classList.remove("show-failed-password");
+    checkPassword[0].classList.add("show-check-password");
+    password.classList.remove("error");
+    password.classList.add("success");
+  }
+
+  if (message.length > 0) {
+    message.innerText = messages.join("\n");
+  }
+  if (message.length === 0) {
+    message.innerText = "Successful login!";
+  }
+});
